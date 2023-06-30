e5790e1c31511b1e6604489439e89a998dd34a3f
vizvasrj <vizvasrj@gmail.com>
2023-06-30 21:35:30 +0530

    templates basic

--- main.go
+++ main.go
@@ -1,5 +1,26 @@
 package main
 
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
 func main() {
+	router := gin.Default()
+
+	files := []string{
+		"./templates/index.html",
+		"./templates/global/base.html",
+		"./templates/global/footer.html",
+		"./templates/global/navbar.html",
+		"./templates/global/js.html",
+	}
+
+	router.LoadHTMLFiles(files...)
+	router.StaticFS("/static", http.Dir("static"))
+
+	router.GET("/", index)
+	router.Run(":80")
 
 }
